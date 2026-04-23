96f89695394f3041ccbc7b68b5d849bce8427acc
xXAlackXx <josedanielfmar@gmail.com>
2026-04-23 01:48:35 -0400

    Add Level, Unit and Lesson models

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Level(models.Model):
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=50)
+    order = models.IntegerField()
+
+
+class Unit(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    order = models.IntegerField()
+    level = models.ForeignKey(Level, on_delete=models.PROTECT)
+
+class Lesson(models.Model):
+    unit = models.ForeignKey(Unit, on_delete=models.PROTECT)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    order = models.IntegerField()
+    difficulty = models.CharField(max_length=20, 
+                                  choices=[('easy', 'Easy'), ('medium', 'Medium'), ('hard', 'Hard')])
