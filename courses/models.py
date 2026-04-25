95ace61124a77137a466b5897209fad274e28e7a
xXAlackXx <josedanielfmar@gmail.com>
2026-04-25 01:55:37 -0400

    Add Vocabulary and LessonVocabulary models

--- courses/models.py
+++ courses/models.py
@@ -21,3 +21,18 @@ class Lesson(models.Model):
     order = models.IntegerField()
     difficulty = models.CharField(max_length=20, 
                                   choices=[('easy', 'Easy'), ('medium', 'Medium'), ('hard', 'Hard')])
+    words = models.ManyToManyField('Vocabulary', through='LessonVocabulary')
+
+class Vocabulary(models.Model):
+    dutch_text = models.CharField(max_length=50)
+    translation = models.CharField(max_length=50, null=True, blank=True)
+    type = models.CharField(max_length=20, 
+                            choices=[('word', 'Word'), ('digraph', 'Digraph')])
+    pronunciation_audio = models.CharField(max_length=250)
+
+class LessonVocabulary(models.Model): 
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20,
+                            choices=[('main', 'Main'), ('example', 'Example')])
+    order = models.IntegerField()
