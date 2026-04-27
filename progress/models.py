b737073f269a1cba9b204c2d5af772beaeb360d7
xXAlackXx <josedanielfmar@gmail.com>
2026-04-26 23:53:50 -0400

    create LessonAttempt model

--- progress/models.py
+++ progress/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from courses.models import Lesson
+from django.conf import settings
 
 # Create your models here.
+class LessonAttempt(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
