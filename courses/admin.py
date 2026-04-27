fcc059be5fcc6e9aac09bdd33f6f31c030bb3144
xXAlackXx <josedanielfmar@gmail.com>
2026-04-27 18:03:12 -0400

    add models to admin and create model in progress app

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Level, Unit, Lesson, Vocabulary, LessonVocabulary
 # Register your models here.
+admin.site.register(Level)
+admin.site.register(Unit)
+admin.site.register(Lesson)
+admin.site.register(Vocabulary)
+admin.site.register(LessonVocabulary)
--- courses/models.py
+++ courses/models.py
@@ -6,6 +6,9 @@ class Level(models.Model):
     code = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
     order = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
 
 
 class Unit(models.Model):
@@ -13,6 +16,10 @@ class Unit(models.Model):
     description = models.TextField()
     order = models.IntegerField()
     level = models.ForeignKey(Level, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
 
 class Lesson(models.Model):
     unit = models.ForeignKey(Unit, on_delete=models.PROTECT)
@@ -23,6 +30,9 @@ class Lesson(models.Model):
                                   choices=[('easy', 'Easy'), ('medium', 'Medium'), ('hard', 'Hard')])
     words = models.ManyToManyField('Vocabulary', through='LessonVocabulary')
 
+    def __str__(self):
+        return self.title
+
 class Vocabulary(models.Model):
     dutch_text = models.CharField(max_length=50)
     translation = models.CharField(max_length=50, null=True, blank=True)
@@ -30,9 +40,21 @@ class Vocabulary(models.Model):
                             choices=[('word', 'Word'), ('digraph', 'Digraph')])
     pronunciation_audio = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.dutch_text
+    
+    class Meta:
+        verbose_name_plural = "Vocabularies"
+
 class LessonVocabulary(models.Model): 
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
     role = models.CharField(max_length=20,
                             choices=[('main', 'Main'), ('example', 'Example')])
     order = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.lesson.title} - {self.vocabulary.dutch_text} ({self.role})"
+    
+    class Meta:
+        verbose_name_plural = "Lesson Vocabularies"
